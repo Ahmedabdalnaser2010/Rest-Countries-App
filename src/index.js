f5a404f964aeaa172181ac7550d3081e2c1341ca
Ahmed Gamal <ahmedabdelnaser3010@gmail.com>
2024-11-29 00:17:08 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 
 
 
